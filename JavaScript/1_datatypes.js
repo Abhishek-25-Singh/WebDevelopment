e935363c2f5c9ff380f19462ccb602190502ea49
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-10-30 23:57:53 +0530

    Types of Data Type in javaScript
                               👉 Primitive-Data Type  👈
                                           🔹 number
                                           🔹 String
                                           🔹 Boolean
                                           🔹 null 
                                           🔹 undefined
                                           🔹 BigInt
                                           🔹 Symbol
                               👉 NoN Primitive-Data Type  👈
                                           🔹 Object
                                           🔹 Array
                                           🔹Function

--- JavaScript/1_datatypes.js
+++ JavaScript/1_datatypes.js
@@ -3,3 +3,27 @@
 //* ============================
 
 //* Data types define the type of values that a variable can hold.
+//? Types of Primitive Data types
+
+//? Number: Represents numeric values, including integers and floating-point numbers.
+// Example:
+
+//? String: Represents a sequence of characters enclosed in single or double quotes.
+// Example:
+
+//? Boolean: Represents a logical entity with two values: true or false.
+// Example:
+
+//? undefined: Represents the absence of a value or an uninitialized variable.
+// Example:
+
+//? Null: Represents the absence of a value. It is often used to explicitly indicate that a variable or object property has no assigned value.
+// Example:
+
+//? BigInt: Represents integers of arbitrary precision (available since ECMAScript 2020).
+// Example:
+// const bigNumber = 1234567890123456789012345678901234567890n;
+
+//? Symbol: Represents a unique and immutable data type, often used to create unique identifiers.
+// Example:
+// const mySymbol = Symbol("description");
