473fd59e3eacfdb581ac6b6450f56942aaf7b699
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-11-15 20:27:26 +0530

    Assignment operators
                            👉     Assignment operators     👈
    🔹var myFavNum = 15;
              🔸 var is the keyword
              🔸 myFavNum is a Variable Name
              🔸 Value of the variable 
    📍Assign The Value 15 to the Variable myFavNum

--- JavaScript/3_expression.js
+++ JavaScript/3_expression.js
@@ -3,3 +3,12 @@
 //* ====================================
 //? 1st we will see what is expression means and also what is operand and operator in any expression?
 
+//* ===================================
+//* 1: Assignment operators
+//* ====================================
+
+// examples
+var x = 5;
+var y = 10;
+var myFavNum = 15;
+var channelName = "Star Movie Studio ";
