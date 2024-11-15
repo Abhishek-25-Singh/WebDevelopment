a57ea446e3567eef5a07d23de82da830d741bcdf
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-11-15 20:23:17 +0530

    what is expression ?
                                    🔹 5 + 20   =  25
    🔸 5 ---> Operand
    🔸 + ---> Operator
    🔸 20 ---> Operand
    
    📍Combination of Operand's and Operator are Called Expression.

--- JavaScript/3_expression.js
+++ JavaScript/3_expression.js
@@ -1,3 +1,5 @@
 //* ===================================
 //*  EXPRESSIONS AND OPERATORS Sections
 //* ====================================
+//? 1st we will see what is expression means and also what is operand and operator in any expression?
+
