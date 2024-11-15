e3e56d533ac67cb41216e7741aba763df870f8a5
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-11-15 20:32:08 +0530

    Arithmetic operators
                                     👉  Arithmetic operators  👈
    🔹Arithmetic operators in programming perform basic 
         mathematical operations on variables or values. 
    🔹They include addition, subtraction, multiplication, division, 
          and modulus.
    
    🔸 Addition (+)
    🔸 Subtraction (-)
    🔸 Multiplication (*)
    🔸 Division (/)
    🔸 Modulus (%)

--- JavaScript/3_expression.js
+++ JavaScript/3_expression.js
@@ -12,3 +12,39 @@ var x = 5;
 var y = 10;
 var myFavNum = 15;
 var channelName = "Star Movie Studio ";
+
+
+//* ===================================
+//* 2: Arithmetic operators
+//* ====================================
+
+//? Addition (+): Adds two values or variables.
+// Example:
+// let x = 5;
+// let y = 10;
+// let sum = x + y;
+// console.log(sum);
+
+//? Subtraction (-): Subtracts the right operand from the left operand.
+// Example:
+// let a = 10;
+// let b = 7;
+// let difference = a - b;
+
+//? Multiplication (*): Multiplies two values or variables.
+// Example:
+// let p = 4;
+// let q = 6;
+// let product = p * q;
+
+//? Division (/): Divides the left operand by the right operand.
+// Example:
+// let m = 15;
+// let n = 3;
+// let quotient = m / n;
+
+//? Modulus (%): Returns the remainder when the left operand is divided by the right operand.
+// Example:
+// let c = 17;
+// let d = 5;
+// let remainder = c % d;
