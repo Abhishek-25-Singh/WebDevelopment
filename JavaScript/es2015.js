e2ca5c9898987ddf243237d0163f3862eab7625c
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-12-03 17:11:26 +0530

    let Keyword
    🔹 The let Keyword is used to declare variable with block Scope.
    🔹 Variables declared with let are mutable, meaning their values 
          can be reassigned.

---
+++ JavaScript/es2015.js
@@ -0,0 +1,18 @@
+//* ======================================
+//* Modern JavaScript - EcmaScript 2015
+//* =====================================
+
+//* ======================================
+//* LET AND CONST - EcmaScript 2015
+//* =====================================
+
+
+                   // *Var Keyword  
+// var MyFavWorldBestName = "Abhishek";
+//     MyFavWorldBestName = "Abhishek Singh";
+//     console.log(MyFavWorldBestName);
+
+                // *let Keyword  
+// let MyFavWorldBestName = "Abhishek";
+//     MyFavWorldBestName = "Abhishek Singh";
+//     console.log(MyFavWorldBestName);
